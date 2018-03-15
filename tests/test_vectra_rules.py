43174545aa17609784ab9455f8f6f37c57be7b9c
cjohnson <cjohnson@vectra.ai>
2018-03-15 10:57:28 -0500

    host2 fixture; replace/append rules

--- tests/test_vectra_rules.py
+++ tests/test_vectra_rules.py
@@ -14,10 +14,16 @@ def test_host(vc_v2):
     return vc_v2.get_hosts().json()['results'][-1]
 
 
+@pytest.fixture()
+def test_host2(vc_v2):
+    return vc_v2.get_hosts().json()['results'][-2]
+
+
+# TODO Group tests into classes; host, ip, sensor_luid, all_hosts
+
 def test_create_rule_host(vc_v2, test_host):
-    host_url = test_host['url']
     resp = vc_v2.create_rule(detection_category='botnet activity', detection_type='bitcoin mining',
-                             triage_category='misconfiguration', description='pytest_hostname', host=[host_url],
+                             triage_category='misconfiguration', description='pytest_hostname', host=[test_host['url']],
                              remote1_dns=['google.com'], remote1_ip=['8.8.8.8'], remote1_port=['443'])
     test_vars['host_rule_id'] = resp.json().get('id', None)
     assert resp.status_code == 201
@@ -44,19 +50,36 @@ def test_create_rule_all_hosts(vc_v2):
     assert resp.status_code == 201
 
 
-def test_update_rule_replace(vc_v2):
-    pass
-
-
-def test_update_rule_append(vc_v2):
-    pass
-
-
 def test_get_rules(vc_v2):
     # get all rules
     # get rule by id
     # get rule by name
     pass
+
+
+def test_update_rule_replace(vc_v2, test_host2):
+    host_url = test_host2['url']
+    resp = vc_v2.update_rule(rule_id=test_vars['host_rule_id'], host=[host_url], remote1_dns=['foo.com'],
+                             remote1_ip=['4.4.4.4'], remote1_port=['8443'])
+    assert resp.status_code == 200
+
+    resp2 = vc_v2.get_rule(rule_id=test_vars['host_rule_id']).json()
+    assert [host_url] ==  resp2['host']
+    assert ['foo.com'] == resp2['remote1_dns']
+    assert ['4.4.4.4'] == resp2['remote1_ip']
+    assert ['8443'] == resp2['remote1_port']
+
+
+def test_update_rule_append(vc_v2, test_host):
+    resp = vc_v2.update_rule(rule_id=test_vars['host_rule_ip'], append = True, ip = ['254.254.254.254'],
+                             remote1_dns=['foo.com'], remote1_ip=['4.4.4.4'], remote1_port=['8443'])
+    assert resp.status_code == 200
+
+    resp2 = vc_v2.get_rules(rule_id=test_vars['host_rule_ip'])
+    assert all([test_host['last_source'], '254.254.254.254']) in resp2['ip']
+    assert all(['google.com', 'foo.com']) in resp2['remote1_dns']
+    assert all(['8.8.8.8', '4.4.4.4']) in resp2['remote1_ip']
+    assert all(['443', '8443']) in resp2['remote1_port']
 
 
 def test_delete_rule(vc_v2):
