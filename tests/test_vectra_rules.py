539faeafd0f8c6f2ec41aa9b3b5fac82802de860
cjohnson <cjohnson@vectra.ai>
2018-04-02 12:22:03 -0500

    fix rule type/details; fix method call; update bitcoin detection type

--- tests/test_vectra_rules.py
+++ tests/test_vectra_rules.py
@@ -22,7 +22,7 @@ def test_host2(vc_v2):
 # TODO Group tests into classes; host, ip, sensor_luid, all_hosts
 
 def test_create_rule_host(vc_v2, test_host):
-    resp = vc_v2.create_rule(detection_category='botnet activity', detection_type='bitcoin mining',
+    resp = vc_v2.create_rule(detection_category='botnet activity', detection_type='cryptocurrency mining',
                              triage_category='misconfiguration', description='pytest_hostname', host=[test_host['url']],
                              remote1_dns=['google.com'], remote1_ip=['8.8.8.8'], remote1_port=['443'])
     test_vars['host_rule_id'] = resp.json().get('id', None)
@@ -43,9 +43,9 @@ def test_create_rule_sensor_luid(vc_v2):
 
 
 def test_create_rule_all_hosts(vc_v2):
-    resp = vc_v2.create_rule(detection_category='botnet activity', detection_type='abnormal ad activity',
-                             triage_category='misconfiguration', description='pytest_ip', all_hosts=True,
-                             remote1_dns=['google.com'], remote1_ip=['8.8.8.8'], remote1_port=['443'])
+    resp = vc_v2.create_rule(detection_category='botnet activity', detection_type='outbound port sweep',
+                             triage_category='misconfiguration', description='pytest_all', all_hosts=True,
+                             remote1_port=['443'])
     test_vars['host_rule_all_hosts'] = resp.json().get('id', None)
     assert resp.status_code == 201
 
@@ -63,23 +63,24 @@ def test_update_rule_replace(vc_v2, test_host2):
                              remote1_ip=['4.4.4.4'], remote1_port=['8443'])
     assert resp.status_code == 200
 
-    resp2 = vc_v2.get_rule(rule_id=test_vars['host_rule_id']).json()
-    assert [host_url] ==  resp2['host']
+    resp2 = vc_v2.get_rules(rule_id=test_vars['host_rule_id']).json()
+    assert [host_url] == resp2['host']
     assert ['foo.com'] == resp2['remote1_dns']
     assert ['4.4.4.4'] == resp2['remote1_ip']
     assert ['8443'] == resp2['remote1_port']
 
 
 def test_update_rule_append(vc_v2, test_host):
-    resp = vc_v2.update_rule(rule_id=test_vars['host_rule_ip'], append = True, ip = ['254.254.254.254'],
+    resp = vc_v2.update_rule(rule_id=test_vars['host_rule_ip'], append = True, ip=['254.254.254.254'],
                              remote1_dns=['foo.com'], remote1_ip=['4.4.4.4'], remote1_port=['8443'])
     assert resp.status_code == 200
 
-    resp2 = vc_v2.get_rules(rule_id=test_vars['host_rule_ip'])
-    assert all([test_host['last_source'], '254.254.254.254']) in resp2['ip']
-    assert all(['google.com', 'foo.com']) in resp2['remote1_dns']
-    assert all(['8.8.8.8', '4.4.4.4']) in resp2['remote1_ip']
-    assert all(['443', '8443']) in resp2['remote1_port']
+    resp2 = vc_v2.get_rules(rule_id=test_vars['host_rule_ip']).json()
+    print resp2
+    assert all(ip in resp2['ip'] for ip in [test_host['last_source'], '254.254.254.254'])
+    assert all(domain in resp2['remote1_dns'] for domain in ['google.com', 'foo.com'])
+    assert all(ip in resp2['remote1_ip'] for ip in ['8.8.8.8', '4.4.4.4'])
+    assert all(port in resp2['remote1_port'] for port in ['443', '8443'])
 
 
 def test_delete_rule(vc_v2):
