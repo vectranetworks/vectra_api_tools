a6541baf1ec58345e089b90ace66c7cce18bfe7b
cjohnson <cjohnson@vectra.ai>
2018-04-27 15:15:51 -0500

    check page size; compare basic and advance detection search

--- test/test_vectra_adv_search.py
+++ test/test_vectra_adv_search.py
@@ -8,8 +8,8 @@ if not pytest.config.getoption('--token'):
 
 
 def test_critical_quad_host_count(vc_v2):
-    basic_count = vc_v2.get_hosts(certainty_gte=50, threat_gte=50)
-    adv_count = len(vc.advanced_search(stype='hosts', query='host.certainty:>=50 and host.threat:>=50').json()['hosts'])
+    basic_count = vc_v2.get_hosts(certainty_gte=50, threat_gte=50).json()['count']
+    adv_count = len(vc_v2.advanced_search(stype='hosts', query='host.certainty:>=50 and host.threat:>=50').json()['results'])
 
     assert adv_count == basic_count
 
@@ -17,13 +17,25 @@ def test_critical_quad_host_count(vc_v2):
 def test_ip_search(vc_v2):
     test_ip = vc_v2.get_hosts().json()['results'][-1]['last_source']
     basic_host_id = vc_v2.get_hosts(last_source=test_ip).json()['results'][0]['id']
-    adv_host_id = vc_v2.advanced_search(stype='hosts', query='host.last_source:{ip}'.format(ip=test_ip)).json()['hosts'][0]['id']
+    adv_host_id = vc_v2.advanced_search(stype='hosts', query='host.last_source:{ip}'.format(ip=test_ip)).json()['results'][0]['id']
 
     assert adv_host_id == basic_host_id
 
 
-def test medium_and_critical_detection_count(vc_v2):
-    basic_count = vc_v2.get_detections(certainty_gte=50).json()['count']
-    adv_count = len(vc_v2.advanced_search(stype='detections', query='detection.certainty:>=50').json()['detections']
+def test_page_size(vc_v2):
+    ret_objects = len(vc_v2.advanced_search(stype='hosts', query='host.state:"active" OR host.state:"inactive"').json()['results'])
+    assert ret_objects == 50
 
-    assert adv_count == basic_count
+    ret_objects = len(vc_v2.advanced_search(stype='hosts', page_size=100, query='host.state:"active" OR host.state:"inactive"').json()['results'])
+    assert ret_objects == 100
+
+    ret_objects = len(vc_v2.advanced_search(stype='hosts', page_size=17, query='host.state:"active" OR host.state:"inactive"').json()['results'])
+    assert ret_objects == 17
+
+
+def test_medium_and_critical_detection_count(vc_v2):
+    basic_count = vc_v2.get_detections(certainty_gte=50).json()['count']
+    adv_count = len(vc_v2.advanced_search(stype='detections', page_size=5000, query='detection.certainty:>=50').json()['results'])
+
+    # basic search returns triaged detections
+    assert adv_count <= basic_count
