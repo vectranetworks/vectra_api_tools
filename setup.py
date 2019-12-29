e5971572523022ffee33245ebb277344ae1661fd
Harrison Golden <gharryg@mac.com>
2019-12-29 13:02:03 -0600

    Merge remote-tracking branch 'upstream/master'

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='vectra-api-tools',
     description='Vectra API client library',
     long_description=long_desc,
-    version='1.0rc7',
+    version='1.1rc0',
     author='Vectra',
     author_email='tme@vectra.ai',
     url='https://github.com/vectranetworks/vectra_api_tools',
