ea9ccc77f2869e13f1c77c828391075792ea2c6b
aurelienhess <38730075+aurelienhess@users.noreply.github.com>
2020-05-19 11:46:36 +0200

    Update to python 3

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ long_desc="""
 _Vectra API Tools_ is set of resources that is designed to save time and repetitive work by providing a python library that simplifies interaction with the Vectra API. Current modules available:  
     - _cli.py_ is a set of common parameters which can be imported into scripts which are designed to be run from the command line
     - _stix_taxii.py_ is a module that provides a taxii client to ingest threat feeds and write to STIX file
-    - _vectra.py_ is module that provides methods that simplify interaction with the Vectra API. There are methods to support most entities including hosts, detections, and advance search.
+    - _vectra.py_ is module that provides methods that simplify interaction with the Vectra API. There are methods to support most entities including hosts, accounts, detections, and advanced search.
 """
 
 setup(
     name='vectra-api-tools',
     description='Vectra API client library',
     long_description=long_desc,
-    version='1.1rc0',
+    version='2.0rc0',
     author='Vectra',
     author_email='tme@vectra.ai',
     url='https://github.com/vectranetworks/vectra_api_tools',
@@ -21,15 +21,16 @@ setup(
     },
     packages=['vat'],
     install_requires=['requests', 'pytz', 'cabby', 'stix'],
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Utilities'
     ]
 )
